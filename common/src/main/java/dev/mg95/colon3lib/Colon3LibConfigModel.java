ae8dcb72502685bdbf686e2201323b65def93147
mg95 <66237187+themg95@users.noreply.github.com>
2024-11-20 11:26:02 +0300

    remove leftover debug info lol

--- common/src/main/java/dev/mg95/colon3lib/Colon3LibConfigModel.java
+++ common/src/main/java/dev/mg95/colon3lib/Colon3LibConfigModel.java
@@ -8,23 +8,4 @@ import dev.mg95.colon3lib.config.v2.Slider;
 @ConfigModel(id = "colon3lib", name = "Colon3LibConfig")
 public class Colon3LibConfigModel {
     public boolean isGay = true;
-    public String sex = "gay";
-    public int howgay = 100;
-    @Nested
-    public static class amog {
-        public boolean sus = true;
-        @Nested
-        public static class amog2 {
-            public boolean sus = true;
-        }
-    }
-    @Slider(min = 0, max = 100)
-    public int howgaypercent = 100;
-
-    @DoubleSlider(min = 1.5, max = 2)
-    public double howgayprecise = 1.5;
-
-    public double howgayfree = 1.5;
-
-
 }
